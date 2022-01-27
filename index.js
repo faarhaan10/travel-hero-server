5c5def43709d1deae81fd00b86b25f3cc5608a2e
Farhan Elias <faarhaan10@gmail.com>
2022-01-27 18:32:01 +0600

    get single blog

--- index.js
+++ index.js
@@ -82,6 +82,14 @@ async function run() {
             res.send(result);
         });
 
+        // single blogs
+        app.get('/blogs/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await blogCollection.findOne(query);
+            res.send(result);
+        })
+
         /*******************************************\
          -------------all put api's----------------
         \*******************************************/
