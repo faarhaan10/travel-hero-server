25e72dcc849289539e4e3ebb6ce0a78f0036836f
Farhan Elias <faarhaan10@gmail.com>
2022-01-27 13:08:17 +0600

    mongodb connection

--- index.js
+++ index.js
@@ -11,6 +11,26 @@ app.use(cors());
 app.use(express.json());
 
 
+//mongodb connection tools
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.y4qnm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        // db collections
+
+
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
 //default api's
 app.get('/', (req, res) => {
     res.send('Databse is live');
